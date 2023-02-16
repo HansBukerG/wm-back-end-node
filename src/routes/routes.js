44644d5eb7dd08767d6af41003d92943a95e3cbc
Hans Buker <hansbuker1993@gmail.com>
2023-02-16 18:45:30 -0300

    SP: Before release.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,10 @@
 
 import { Router } from 'express'
-import {read, readById } from '../Controllers/Product.Controller.js'
+import {read, readById, readByString } from '../Controllers/Product.Controller.js'
 const RouterProduct = Router();
 
 RouterProduct.get('/search/', read);
-RouterProduct.get('/search/:id', readById);
+RouterProduct.get('/search/:id', readByString);
+RouterProduct.get('/search/id/:id', readById);
 
 export { RouterProduct };
